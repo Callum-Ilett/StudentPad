79ce6253c3d1d6b390fc030b1e5cfe4ac6eea22c
Ilettc <callum_i@yahoo.com>
2021-01-15 13:23:08 +0000

    Added deployment settings

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import routes from "./routes/routes.js";
-
+import path from "path";
 import "./config/mongoose.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -12,7 +17,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.set("etag", false);
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use("/api", routes);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(port, () =>
   console.log(`Listening on http://localhost:${port}/api`)
