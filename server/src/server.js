a9616899b372b6d1a6417f734d4ea53bb7ca4403
Ilettc <callum_i@yahoo.com>
2021-01-15 15:42:42 +0000

    Fixed deployment finally

--- server/src/server.js
+++ server/src/server.js
@@ -10,16 +10,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
 app.set("etag", false);
 
-// app.use("/", express.static(__dirname + "/public"));
-app.use("/", express.static("/public"));
-// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../../client/build")));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+});
+
 app.use("/api", routes);
 
 // // The "catchall" handler: for any request that doesn't
